6445c4e9fcbea23d5e356ec6512ce7944da6b2f9
Niklas Klein <my.taig@gmail.com>
2014-06-05 17:03:13 +0200

    Updated to play 2.3

--- project/Build.scala
+++ project/Build.scala
@@ -1,12 +1,17 @@
+import play.Play.autoImport._
+import play.PlayScala
+import PlayKeys._
 import sbt._
 import sbt.Keys._
 
 object Build extends sbt.Build
 {
-	lazy val main = play.Project( "taig-io" )
+	lazy val main = Project( "taig-io", file( "." ) )
+		.enablePlugins( PlayScala )
 		.settings(
 			libraryDependencies ++= Seq( "com.mohiva" %% "play-html-compressor" % "0.2.1" ),
 			name := "taig.io",
+			scalaVersion := "2.11.1",
 			version := "1.0.0"
 		)
 }
