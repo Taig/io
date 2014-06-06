0bb45d7537ae85a387b030ed4e08e4decb1784cc
Niklas Klein <my.taig@gmail.com>
2014-06-06 03:30:05 +0200

    Updated to play-html-compressor 0.3

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Build extends sbt.Build
 	lazy val main = Project( "taig-io", file( "." ) )
 		.enablePlugins( PlayScala )
 		.settings(
-			libraryDependencies ++= Seq( "com.mohiva" %% "play-html-compressor" % "0.2.1" ),
+			libraryDependencies ++= Seq( "com.mohiva" %% "play-html-compressor" % "0.3" ),
 			name := "taig.io",
 			scalaVersion := "2.11.1",
 			version := "1.0.0"
