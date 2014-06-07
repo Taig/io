1bb49ac48695e8abef36b885bea9044016dbef63
Niklas Klein <my.taig@gmail.com>
2014-06-07 21:54:37 +0200

    Switched to using webjars and load jquery accordingly

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,10 @@ object Build extends sbt.Build
 	lazy val main = Project( "taig-io", file( "." ) )
 		.enablePlugins( PlayScala )
 		.settings(
-			libraryDependencies ++= Seq( "com.mohiva" %% "play-html-compressor" % "0.3" ),
+			libraryDependencies ++= Seq(
+				"com.mohiva" %% "play-html-compressor" % "0.3",
+				"org.webjars" % "jquery" % "2.1.1"
+			),
 			name := "taig.io",
 			scalaVersion := "2.11.1",
 			version := "1.0.0"
