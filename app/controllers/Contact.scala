dc6396b80fed30fef0c30e21651850a941a5c917
Niklas Klein <my.taig@gmail.com>
2014-06-27 14:32:52 +0200

    Prepared contact form validation

--- app/controllers/Contact.scala
+++ app/controllers/Contact.scala
@@ -3,6 +3,9 @@ package controllers
 import play.api.mvc._
 import views.html.page.contact
 import models.Navigation.Item
+import com.typesafe.plugin._
+import play.api.data._
+import play.api.data.Forms._
 
 object Contact extends Page
 {
@@ -10,8 +13,23 @@ object Contact extends Page
 
 	override val item = Item( "Contact", routes.Contact.index() )
 
-	def index = Action
+	val form = Form(
+		mapping(
+			"name" -> optional( text ),
+			"mail" -> optional( email ),
+			"message" -> nonEmptyText
+		)( Details.apply )( Details.unapply )
+	)
+
+	def index( implicit form: Option[Form[Details]] = None ) = Action
 	{
-		implicit request => Ok( contact.index() )
+		implicit request =>
+		{
+			Ok( contact.index( form ) )
+		}
 	}
+
+	def submit = TODO()
+
+	case class Details( name: Option[String], mail: Option[String], message: String )
 }
